90780d5109109146f008aa256782b118fdd5738d
danogentili <daniil.gentili.dg@gmail.com>
2016-08-15 20:48:20 +0200

    Fixed syntax mistake

--- video.sh
+++ video.sh
@@ -5,10 +5,8 @@
 #
 # Copyright 2016 Daniil Gentili (https://daniil.it)
 # This file is part of video-dl.
-# video-dl is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as 
-published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-# video-dl is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# video-dl is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# video-dl is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU Affero General Public License for more details.
 # You should have received a copy of the GNU General Public License along with the video-dl.
 # If not, see <http://www.gnu.org/licenses/>.
